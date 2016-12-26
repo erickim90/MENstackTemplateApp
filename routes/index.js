5ac20968ead6099a62ca7acdb0b16aa2981c0718
eckim90 <eric@bluepandalabs.com>
2016-12-26 15:30:16 -0800

    Separate Sandbox mode for testing various APIs and Scripts

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,4 @@ router.get('/', function(req, res) {
   res.render('main', pageData);
 });
 
-//TODO add PASSPORT/OAUTH or some security tool for secure Log In
-// router.get('/login', function(req, res) {
-// });
-// router.post('/login', function(req, res) {
-// });
-// router.get('/logout', function(req, res) {
-// });
 module.exports = router;
